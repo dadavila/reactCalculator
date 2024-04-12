7c809acc3ddc5c51a2c196b16843596d70e8276f
Pedroabz3 <pedrobonillazamora@gmail.com>
2024-04-11 23:02:44 -0500

    cambie la pene

--- Caluladora/calculator/src/App.js
+++ Caluladora/calculator/src/App.js
@@ -6,7 +6,7 @@ import { Principal } from './components/Principal/Principal';
 function App() {
   return (
     <div>
-      <h1>CalculDora</h1>
+      <h1>CalculPene</h1>
       <Principal />
     </div>
 
